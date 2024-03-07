df681ddeb05199166f0b160604e89889bb677485
Tobias Skog <t_ott_e@hotmail.com>
2024-03-07 13:19:16 +0100

    merged two backend methods for querying the database and made sure the frontend response handled the data correctly

--- api/routes/databases/PeopleDB/people.js
+++ api/routes/databases/PeopleDB/people.js
@@ -3,152 +3,108 @@ const express = require("express");
 const router = express.Router();
 const notionClient = require("../../../components/notionClient");
 
-async function DBPeopleIsExistingUser(userEmail) {
+async function FindUserInDB(email, password, loginType) {
 	let userData = {
 		isValidUser: false,
 		userRole: "Invalid",
 		id: "",
 		name: "",
 	};
+	let response;
 
-	if (!userEmail || userEmail === "") {
+	if (email === "" || loginType === "") {
+		return userData;
+	}
+
+	if (password === "" && loginType === "Integrated") {
 		return userData;
 	}
 	try {
-		const response = await notionClient.databases.query({
-			database_id: process.env.NOTION_DB_PEOPLE_ID,
-			filter: {
-				property: "Email",
-				email: {
-					equals: userEmail,
-				},
-			},
-		});
+		switch (loginType) {
+			case "Integrated":
+				response = await notionClient.databases.query({
+					database_id: process.env.NOTION_DB_PEOPLE_ID,
+					filter: {
+						and: [
+							{
+								property: "Email",
+								email: {
+									equals: email,
+								},
+							},
+							{
+								property: "Password",
+								rich_text: {
+									equals: password,
+								},
+							},
+						],
+					},
+				});
 
-		if (response.results.length > 0) {
-			return {
-				isValidUser: true,
-				userRole: response.results[0].properties.Role.select.name,
-				id: response.results[0].id,
-			};
-		} else {
-			return {
-				isValidUser: false,
-				userRole: "Invalid",
-				id: "Nan",
-			};
-		}
-	} catch (error) {
-		console.error("Error Query People Table:", error.message);
-		throw error;
-	}
-}
+				break;
 
-async function DBPeopleIsExistingUser2(userEmail, userPassword) {
-	if (userEmail === "") {
-		return {
-			isValidUser: false,
-			userRole: "Invalid",
-			id: "",
-			name: "",
-		};
-	}
-
-	try {
-		const response = await notionClient.databases.query({
-			database_id: process.env.NOTION_DB_PEOPLE_ID,
-			filter: {
-				and: [
-					{
+			case "OAuth":
+				response = await notionClient.databases.query({
+					database_id: process.env.NOTION_DB_PEOPLE_ID,
+					filter: {
 						property: "Email",
 						email: {
-							equals: userEmail,
+							equals: email,
 						},
 					},
-					{
-						property: "Password",
-						rich_text: {
-							equals: userPassword,
-						},
-					},
-				],
-			},
-		});
+				});
 
-		if (response.results.length > 0) {
-			return {
-				id: response.results[0].id,
-				name: response.results[0].properties.Name.title[0].text.content,
-				userRole: response.results[0].properties.Role.select.name,
-				isValidUser: true,
-			};
-		} else {
-			return {
-				isValidUser: false,
-				userRole: "Invalid",
-				id: "",
-				name: "",
-			};
+				break;
 		}
+
+		if (!response.results.length > 0) {
+			return userData;
+		}
+    
+		return (userData = {
+			id: response.results[0].id,
+			name: response.results[0].properties.Name.title[0].text.content,
+			userRole: response.results[0].properties.Role.select.name,
+			isValidUser: true,
+		});
 	} catch (error) {
 		console.error("Database Error:", error.message);
-		res
-			.status(500)
-			.send("Internal server error during database query:", error.message);
+		throw error(error);
 	}
 }
 
 router.post("/login/integratedUser", async (req, res) => {
 	try {
-		const isExistingUser = await DBPeopleIsExistingUser2(
+		const isExistingUser = await FindUserInDB(
 			req.body.userEmail,
-			req.body.userPassword
+			req.body.userPassword,
+			"Integrated"
 		);
-
-		return res.send({
-			isValidUser: isExistingUser.isValidUser,
-			userRole: isExistingUser.userRole,
-			id: isExistingUser.id,
-			name: isExistingUser.name,
-		});
+		res.send(isExistingUser);
 	} catch (error) {
 		console.error("Database Error:", error.message);
 		res
 			.status(500)
-			.send("Internal server error during database query:", error.message);
+			.send(
+				"Internal server error during people database query Integrated:" +
+					error.message
+			);
 	}
 });
 
 router.post("/login/authUser", async (req, res) => {
 	try {
-		const isExistingUser = await DBPeopleIsExistingUser(req.body.userEmail);
-
-		return res.send({
-			isValidUser: isExistingUser.isValidUser,
-			userRole: isExistingUser.userRole,
-			id: isExistingUser.id,
-		});
-	} catch (error) {
-		console.error("Error Query People Table:", error.message);
-		res
-			.status(500)
-			.send("Internal server error during database query:", error.message);
-	}
-});
-
-router.post("/login/authUser", async (req, res) => {
-	try {
-		const isExistingUser = await DBPeopleIsExistingUser(req.body.userEmail);
-		return res.send({
-			isValidUser: isExistingUser.isValidUser,
-			userRole: isExistingUser.userRole,
-			id: isExistingUser.id,
-		});
+		const isExistingUser = await FindUserInDB(req.body.userEmail, "", "OAuth");
+		res.send(isExistingUser);
 	} catch (error) {
 		console.error("Database Error:", error.message);
 		res
-			.status(520)
-			.send("Internal server error during database query:", error.message);
+			.status(500)
+			.send(
+				"Internal server error during people database query OAuth:" +
+					error.message
+			);
 	}
 });
 
