09f11990e2d7bdb4673fb4d308249983f53ebfd4
Tobias Skog <t_ott_e@hotmail.com>
2024-03-07 10:17:40 +0100

    preped for merge

--- api/routes/databases/databases.js
+++ api/routes/databases/databases.js
@@ -1,61 +1,67 @@
 require("dotenv").config();
 const express = require("express");
 const router = express.Router();
-const notionClient = require("../components/notionClient");
+const notionClient = require("../../components/notionClient");
 
 async function DBPeopleIsExistingUser(userEmail) {
+	let userData = {
+		isValidUser: false,
+		userRole: "Invalid",
+		id: "",
+		name: "",
+	};
+
 	if (userEmail === "") {
-		return {
-			isValidUser: false,
-			userRole: "Invalid",
-		};
+		return userData;
 	}
 
 	try {
 		const response = await notionClient.databases.query({
 			database_id: process.env.NOTION_DB_PEOPLE_ID,
 			filter: {
-				or: [
+				and: [
 					{
 						property: "Email",
 						email: {
 							equals: userEmail,
 						},
 					},
+					{
+						property: "Password",
+						rich_text: {
+							equals: userPasswowrd,
+						},
+					},
 				],
 			},
 		});
 
-		if (response.results.length > 0) {
-			return {
-				isValidUser: true,
-				userRole: response.results[0].properties.Role.select.name,
-			};
-		} else {
-			return {
-				isValidUser: false,
-				userRole: "Invalid",
-			};
+		if (!response.results.length > 0) {
+			return userData;
 		}
+
+		userData.isValidUser = true;
+		userData.userRole = response.results[0].properties.Role.select.name;
+		userData.id = response.results[0].id;
+		return userData;
 	} catch (error) {
 		console.error("Error Query People Table:", error.message);
 		throw error;
 	}
 }
 
-router.post("/login/confirmUser", async (req, res) => {
+router.post("/login/authUser", async (req, res) => {
 	try {
-		console.log("USEREMAIL:", req.body.userEmail);
 		const isExistingUser = await DBPeopleIsExistingUser(req.body.userEmail);
-		res.send({
+		return res.send({
 			isValidUser: isExistingUser.isValidUser,
 			userRole: isExistingUser.userRole,
+			id: isExistingUser.id,
 		});
-		return;
 	} catch (error) {
 		console.error("Database Error:", error.message);
 		res
-			.status(500)
+			.status(520)
 			.send("Internal server error during database query:", error.message);
 	}
 });
--- api/routes/login.js
+++ api/routes/login.js
@@ -5,16 +5,18 @@ const axios = require("axios");
 
 router.get("/auth/callback", async (req, res) => {
 	const code = req.query.code;
-	let userData = {
-		email: "",
-		userRole: "Invalid",
-		target: "/",
-	};
-
 	// Check if we got a valid code in the query
 	if (!code || typeof code !== "string") {
 		res.status(400).send(userData, "Invalid or missing code parameter");
 	}
+
+	// Create a dataobject to return to frontend
+	let userData = {
+		email: "",
+		name: "",
+		userRole: "Invalid",
+		target: "/",
+	};
 
 	try {
 		let response = await axios.post(
@@ -31,55 +33,27 @@ router.get("/auth/callback", async (req, res) => {
 					username: process.env.NOTION_OAUTH_CLIENT_ID,
 					password: process.env.NOTION_OAUTH_CLIENT_SECRET,
 				},
+				headers: {
+					"Content-Type": "application/json",
+				},
 			}
 		);
-
 		//Check if the response status is NOT 200 (the authentication FAILED)
 		if (response.status !== 200) {
 			console.error("OAuth Token Request Failed. Status:", response.status);
-			res.status(401).send(userData);
+			return res.status(401).send(userData);
 		}
-
-		//  The response was 200 - Control if the authorized user is in our People Database
-
-		// BREAKING THIS OUT TO FRONTEND
-		// let validUser = await axios.post(
-		// 	"http://localhost:3001/databases/login/confirmUser",
-		// 	{
-		// 		userEmail: response.data.owner.user.person.email,
-		// 	}
-		// );
-
-		// Create a dataobject to return to frontend
-
-		// Is the user a Valid User?
-		//if (!validUser.data.isValidUser) {
-		// User is NOT found in the people table and is NOT a valid user
-		//} else {
 		// User IS FOUND in the people table and IS A VALID user
+
 		userData = {
-			accessToken: response.data.access_token,
 			email: response.data.owner.user.person.email,
-			userId: response.data.owner.user.id,
-			botId: response.data.bot_id,
-			userObj: response.data.owner.user,
+			name: response.data.owner.user.name,
 		};
-		//}
 
-		// respond to the original request with the userData object
-		// which will have it's data updated based on the result of
-		// the database query and the authentication
-
-		res.send(userData);
-		// } catch (error) {
-		// 	console.error("Error during user verification process:", error.message);
-		// 	res
-		// 		.status(500)
-		// 		.send("Internal server error during user verification process.");
-		// }
+		return res.send(userData);
 	} catch (error) {
 		console.error("Error during OAuth process:", error.message);
-		res.status(500).send("Internal server error during OAuth process.");
+		return res.status(500).send("Internal server error during OAuth process.");
 	}
 });
 
